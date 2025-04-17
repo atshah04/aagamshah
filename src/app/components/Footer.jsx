2314a7310e2b1c34d3d0ece8422ee24c875e7b65
atshah04 <atshah@uwaterloo.ca>
2025-04-17 00:57:06 -0400

    add fonts

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,13 +1,26 @@
+"use client";
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
-      </div>
-    </footer>
+    <>
+      <style jsx>{`
+        @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap');
+        .caveat-font {
+          font-family: 'Caveat', cursive;
+          font-optical-sizing: auto;
+          font-weight: 400; /* Adjust weight as needed */
+          font-style: normal;
+          font-size: 2rem; /* Increase font size */
+        }
+      `}</style>
+      <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+        <div className="container p-12 flex justify-between">
+          <span className="caveat-font">Aagam Shah</span>
+          <p className="text-slate-600">All rights reserved.</p>
+        </div>
+      </footer>
+    </>
   );
 };
 
